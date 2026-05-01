88f74d24db2d0f970ef8213800a7e2065a81c1b9
LiamG-WashU <g.liam@wustl.edu>
2026-05-01 17:01:47 -0500

    error handling

--- recommendations/recommendations-script.js
+++ recommendations/recommendations-script.js
@@ -6,53 +6,47 @@ const alreadyWatchedMediaList = document.querySelector("#already-watched-media-l
 let data, mediaData, prereqData;
 
 async function fetchData() {
+    let response = await fetch("../data.xml");
+    if(!response.ok) throw new Error("Failed to connect to the server!");
+    let responseXML = await response.text();
+    data = new DOMParser().parseFromString(responseXML, "text/xml");
+    mediaData = data.querySelector("mediaData").children;
+    prereqData = data.querySelector("prereqData").children;
+}
+
+async function getRecommendations() {
     try {
-        let response = await fetch("../data.xml");
-        if(!response.ok) throw new Error("Failed to connect to the server!");
-        let responseXML = await response.text();
-        data = new DOMParser().parseFromString(responseXML, "text/xml");
-        mediaData = data.querySelector("mediaData").children;
-        prereqData = data.querySelector("prereqData").children;
+        await fetchData()
+        if(mediaData && mediaData.length > 0) {
+            for(media of mediaData) {
+                let mediaTitle = media.querySelector("title").textContent;
+                let mediaType = media.querySelector("type").textContent;
+                let mediaElement = document.createElement("ul");
+                mediaElement.textContent = mediaTitle + " (" + mediaType + ")";
+
+                if(localStorage.getItem(mediaTitle) == "true") alreadyWatchedMediaList.appendChild(mediaElement);
+                else {
+                    let hasMajorPrereq = false;
+                    for(let prereq of media.querySelector("majorPrereqs").children) {
+                        if(!prereqSatisfied(prereq)) hasMajorPrereq = true;
+                    }
+                    if(hasMajorPrereq) veryUnrecommendedMediaList.appendChild(mediaElement);
+                    else {
+                        let hasMinorPrereq = false;
+                        for(let prereq of media.querySelector("minorPrereqs").children) {
+                            if(!prereqSatisfied(prereq)) hasMinorPrereq = true;
+                        }
+                        if(hasMinorPrereq) unrecommendedMediaList.appendChild(mediaElement);
+                        else recommendedMediaList.appendChild(mediaElement);
+                    }
+                }
+            }
+        }
+        else throw new Error("We could not access the media list.");
     }
     catch(error) {
         console.error(error);
         alert(error);
-    }
-}
-
-async function getRecommendations() {
-    await fetchData()
-    if(mediaData.length > 0) {
-        for(media of mediaData) {
-            let mediaTitle = media.querySelector("title").textContent;
-            let mediaType = media.querySelector("type").textContent;
-            let mediaElement = document.createElement("ul");
-            mediaElement.textContent = mediaTitle + " (" + mediaType + ")";
-
-            if(localStorage.getItem(mediaTitle) == "true") alreadyWatchedMediaList.appendChild(mediaElement);
-            else {
-                let hasMajorPrereq = false;
-                for(let prereq of media.querySelector("majorPrereqs").children) {
-                    if(!prereqSatisfied(prereq)) hasMajorPrereq = true;
-                }
-                if(hasMajorPrereq) veryUnrecommendedMediaList.appendChild(mediaElement);
-                else {
-                    let hasMinorPrereq = false;
-                    for(let prereq of media.querySelector("minorPrereqs").children) {
-                        if(!prereqSatisfied(prereq)) hasMinorPrereq = true;
-                    }
-                    if(hasMinorPrereq) unrecommendedMediaList.appendChild(mediaElement);
-                    else recommendedMediaList.appendChild(mediaElement);
-                }
-            }
-        }
-    }
-    else {
-        for(let list of [recommendedMediaList, unrecommendedMediaList, veryUnrecommendedMediaList]) {
-            let errorMessage = document.createElement("b");
-            errorMessage.textContent = "Error: we could not access the media list."
-            list.appendChild(errorMessage);
-        }
     }
 }
 
--- watched/watched-script.js
+++ watched/watched-script.js
@@ -3,52 +3,48 @@ const inputWatchedForm = document.querySelector("#input-watched-form");
 let data, mediaData;
 
 async function fetchData() {
+    let response = await fetch("../data.xml");
+    if(!response.ok) throw new Error("Failed to connect to the server!");
+    let responseXML = await response.text();
+    data = new DOMParser().parseFromString(responseXML, "text/xml");
+    mediaData = data.querySelector("mediaData").children;
+}
+
+async function createForm() {
     try {
-        let response = await fetch("../data.xml");
-        if(!response.ok) throw new Error("Failed to connect to the server!");
-        let responseXML = await response.text();
-        data = new DOMParser().parseFromString(responseXML, "text/xml");
-        mediaData = data.querySelector("mediaData").children;
+        await fetchData();
+        if(mediaData && mediaData.length > 0) {
+            for(media of mediaData) {
+                let mediaTitle = media.querySelector("title").textContent;
+                let mediaType = media.querySelector("type").textContent;
+
+                let checkboxDiv = document.createElement("div");
+                inputWatchedForm.appendChild(checkboxDiv);
+                
+                let checkbox = document.createElement("input");
+                checkbox.type = "checkbox";
+                checkbox.setAttribute("id", mediaTitle);
+                checkbox.checked = (localStorage.getItem(checkbox.getAttribute("id")) == "true");
+                checkboxDiv.appendChild(checkbox);
+
+                let checkboxLabel = document.createElement("label");
+                checkboxLabel.setAttribute("for", mediaTitle);
+                checkboxLabel.textContent = mediaTitle + " (" + mediaType + ")";
+                checkboxDiv.appendChild(checkboxLabel);
+                
+                checkbox.addEventListener("click", function() {
+                    for(let currentChild of inputWatchedForm.children) {
+                        currentCheckbox = currentChild.children[0];
+                        localStorage.setItem(currentCheckbox.getAttribute("id"), currentCheckbox.checked);
+                    }
+                });
+            }
+        }
+        else throw new Error("We could not access the media list.");
     }
     catch(error) {
         console.error(error);
         alert(error);
-    }
-}
-
-async function createForm() {
-    await fetchData();
-    if(mediaData.length > 0) {
-        for(media of mediaData) {
-            let mediaTitle = media.querySelector("title").textContent;
-            let mediaType = media.querySelector("type").textContent;
-
-            let checkboxDiv = document.createElement("div");
-            inputWatchedForm.appendChild(checkboxDiv);
-            
-            let checkbox = document.createElement("input");
-            checkbox.type = "checkbox";
-            checkbox.setAttribute("id", mediaTitle);
-            checkbox.checked = (localStorage.getItem(checkbox.getAttribute("id")) == "true");
-            checkboxDiv.appendChild(checkbox);
-
-            let checkboxLabel = document.createElement("label");
-            checkboxLabel.setAttribute("for", mediaTitle);
-            checkboxLabel.textContent = mediaTitle + " (" + mediaType + ")";
-            checkboxDiv.appendChild(checkboxLabel);
-            
-            checkbox.addEventListener("click", function() {
-                for(let currentChild of inputWatchedForm.children) {
-                    currentCheckbox = currentChild.children[0];
-                    localStorage.setItem(currentCheckbox.getAttribute("id"), currentCheckbox.checked);
-                }
-            });
-        }
-    }
-    else {
-        let errorMessage = document.createElement("b");
-        errorMessage.textContent = "Error: we could not access the media list."
-        inputWatchedForm.appendChild(errorMessage);
     }
 }
 
